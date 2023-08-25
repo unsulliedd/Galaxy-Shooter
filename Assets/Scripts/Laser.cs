9aa173d1f5942308811223ac313e4649b95e2fd3
Berk <berkkarakas98@hotmail.com>
2023-08-25 18:43:06 +0300

    Add laser movement and destruction logic

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 8.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(_speed * Time.deltaTime * Vector3.up);
+        if(transform.position.y > 8.0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
