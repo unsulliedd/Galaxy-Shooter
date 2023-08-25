9843134302f70aa3a93d807c20c49d204cc8024d
Berk <berkkarakas98@hotmail.com>
2023-08-25 17:20:14 +0300

    Implement player movement with boundary control and screen wrapping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Not accessible by other scripts
+    [SerializeField]
+    private float _speed = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Set the player's position to the origin
+        transform.position = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        // Get input from the player
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Set the boundaries for the player
+        float VerticalBoundary = 4.0f;
+        float HorizontalBoundary = 11.2f;
+
+        // Create a vector based on the input
+        Vector3 inputDirection = new(horizontalInput, verticalInput, 0);
+
+        // Move the player based on input
+        transform.Translate(_speed * Time.deltaTime * inputDirection);
+
+        // Clamp the player's position vertically
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -VerticalBoundary, VerticalBoundary), 0);
+
+        // Wrap the player's position horizontally
+        if (transform.position.x > HorizontalBoundary)
+        {
+            transform.position = new Vector3(-HorizontalBoundary, transform.position.y, 0);
+        }
+        else if (transform.position.x < -HorizontalBoundary)
+        {
+            transform.position = new Vector3(HorizontalBoundary, transform.position.y, 0);
+        }
     }
 }
