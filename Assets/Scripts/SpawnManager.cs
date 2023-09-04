ecf4247760de5b4ebb80569ea2a487d0dc738ec9
Berk <berkkarakas98@hotmail.com>
2023-09-04 22:49:11 +0300

    Enhance enemy spawning with container management
    - Add enemies to enemy container

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,6 +7,10 @@ public class SpawnManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject _enemyPrefab;
+    [SerializeField]
+    private GameObject _enemyContainer;
+    [SerializeField]
+    private float _spawnRate = 3.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,13 +24,20 @@ public class SpawnManager : MonoBehaviour
 
     }
 
+    // Spawn game objects every 3 seconds
     IEnumerator SpawnRoutine()
     {
         while (true)
         {
+            // Instantiate an enemy prefab
             Vector3 spawnPosition = new Vector3(Random.Range(-8.0f, 8.0f), 7.0f, 0);
-            Instantiate(_enemyPrefab, spawnPosition, Quaternion.identity);
-            yield return new WaitForSeconds(3.0f);
+            GameObject newEnemy = Instantiate(_enemyPrefab, spawnPosition, Quaternion.identity);
+
+            // Set the parent of the new enemy to the enemy container
+            newEnemy.transform.parent = _enemyContainer.transform;
+
+            // Wait for 3 seconds
+            yield return new WaitForSeconds(_spawnRate);
         }
     }
 }
