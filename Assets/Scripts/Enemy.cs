f50f6c1ef9320ad89088edcc6d187bfeec3080ee
Berk <berkkarakas98@hotmail.com>
2023-09-04 20:07:50 +0300

    Implement enemy movement and respawn logic

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]
+    private float _speed = 4.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,12 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(_speed * Time.deltaTime * Vector3.down);
+
+        if(transform.position.y < -6.0f)
+        {
+            float randomXPosition = Random.Range(-8.0f, 8.0f);
+            transform.position = new Vector3(randomXPosition, 7.0f, 0);
+        }
     }
 }
