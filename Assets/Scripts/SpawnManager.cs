5b5f2ab3fb4a6502579beb39f071bf1dda21e6aa
Berk <berkkarakas98@hotmail.com>
2023-09-04 22:40:41 +0300

    Add enemy spawn logic

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _enemyPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        StartCoroutine(SpawnRoutine());
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            Vector3 spawnPosition = new Vector3(Random.Range(-8.0f, 8.0f), 7.0f, 0);
+            Instantiate(_enemyPrefab, spawnPosition, Quaternion.identity);
+            yield return new WaitForSeconds(3.0f);
+        }
     }
 }
