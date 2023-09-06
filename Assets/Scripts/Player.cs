5bb199c8c5e45d0658d23464af62199e1f6921ed
Berk <berkkarakas98@hotmail.com>
2023-09-06 14:44:47 +0300

    Update FireLaser method for triple shot

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float _speed = 4.0f;
-    [SerializeField]
     private GameObject _laserPrefab;
+    [SerializeField]
+    private GameObject _tripleShotPrefab;
+    [SerializeField]
+    private float _speed = 4.0f;
     [SerializeField]
     private float _fireRate = 0.345f;
     private float _nextFire = 0.0f;
     [SerializeField]
     private int _lives = 3;
-    
+    [SerializeField]
+    private bool _isTripleShotActive = false;
+
     private SpawnManager _spawnManager;
 
     // Start is called before the first frame update
@@ -75,7 +79,14 @@ public class Player : MonoBehaviour
     void FireLaser()
     {
         _nextFire = Time.time + _fireRate;
-        Instantiate(_laserPrefab, transform.position + new Vector3(0, 1.05f, 0), Quaternion.identity);
+        if(_isTripleShotActive == true)
+        {
+            Instantiate(_tripleShotPrefab, transform.position, Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(_laserPrefab, transform.position + new Vector3(0, 1.05f, 0), Quaternion.identity);
+        }
     }
 
     public void Damage()
