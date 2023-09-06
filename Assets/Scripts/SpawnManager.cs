1375ebc44c4433085d110acf58b9201e9db71a09
Berk <berkkarakas98@hotmail.com>
2023-09-06 15:24:24 +0300

    Add TripleShot Power-Up spawning routine
    - Spawn routine for TripleShot Power-Up

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,6 +9,8 @@ public class SpawnManager : MonoBehaviour
     [SerializeField]
     private GameObject _enemyContainer;
     [SerializeField]
+    private GameObject _tripleShotPowerUpPrefab;
+    [SerializeField]
     private float _spawnRate = 3.0f;
 
     private bool _stopSpawning = false;
@@ -16,7 +18,8 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnRoutine());
+        StartCoroutine(EnemySpawnRoutine());
+        StartCoroutine(PowerUpSpawnRoutine());
     }
 
     // Update is called once per frame
@@ -26,7 +29,7 @@ public class SpawnManager : MonoBehaviour
     }
 
     // Spawn game objects every 3 seconds
-    IEnumerator SpawnRoutine()
+    IEnumerator EnemySpawnRoutine()
     {
         while (_stopSpawning == false)
         {
@@ -39,6 +42,19 @@ public class SpawnManager : MonoBehaviour
 
             // Wait for 3 seconds
             yield return new WaitForSeconds(_spawnRate);
+        }
+    }
+
+    IEnumerator PowerUpSpawnRoutine()
+    {
+        // Wait 20 seconds before spawning the first power up
+        yield return new WaitForSeconds(20);
+
+        while (_stopSpawning == false)
+        {
+            Vector3 spawnPosition = new Vector3(Random.Range(-8.0f, 8.0f), 7.0f, 0);
+            GameObject tripleShotPowerUp = Instantiate(_tripleShotPowerUpPrefab, spawnPosition, Quaternion.identity);
+            yield return new WaitForSeconds(Random.Range(10, 21));
         }
     }
 
