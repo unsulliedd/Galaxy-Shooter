2813d4485d27f3fa1ac21a8b624bcad4fd23ec39
Berk <berkkarakas98@hotmail.com>
2023-09-06 19:25:48 +0300

    Add "Game Over" text for end game state

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -9,6 +9,8 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI _scoreText;
     [SerializeField]
+    private TextMeshProUGUI _gameOverText;
+    [SerializeField]
     private Image _livesImage;
     [SerializeField]
     private Sprite[] _liveSprites;
@@ -17,6 +19,7 @@ public class UIManager : MonoBehaviour
     void Start()
     {
         _scoreText.text = "Score: " + 0;
+        _gameOverText.gameObject.SetActive(false);
     }
 
     public void UpdateScore(int playerScore)
@@ -27,5 +30,21 @@ public class UIManager : MonoBehaviour
     public void UpdateLives(int currentLive)
     {
         _livesImage.sprite = _liveSprites[currentLive];
+        if (currentLive == 0)
+        {
+            _gameOverText.text = "WASTED";
+            StartCoroutine(GameOverFlickerRoutine());
+        }
+    }
+
+    IEnumerator GameOverFlickerRoutine()
+    {
+        while (true)
+        {
+            _gameOverText.gameObject.SetActive(true);
+            yield return new WaitForSeconds(0.3f);
+            _gameOverText.gameObject.SetActive(false);
+            yield return new WaitForSeconds(0.3f);
+        }
     }
 }
