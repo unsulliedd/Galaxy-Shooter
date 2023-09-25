702fcd1aded1eb22e23105b83f6d182d6f73723d
Berk <berkkarakas98@hotmail.com>
2023-09-25 18:51:36 +0300

    Add settings menu design

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -3,6 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _mainMenu;
+    [SerializeField]
+    private GameObject _settingsMenu;
     public void LoadGame()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -12,5 +16,11 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void OpenSettings()
+    {
+        _mainMenu.SetActive(false);
+        _settingsMenu.SetActive(true);
     }
 }
